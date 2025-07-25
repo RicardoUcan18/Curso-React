e9ffdf6eae6963f893e0d6e4aba317f0196ac80c
RicardoUcan18 <ricardoprog0@gmail.com>
2025-07-25 14:22:21 -0600

    - seccion 4

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+export const getResultado = () => { return 1 + 1 };
+
+export const FirstApp = ({titulo = 'titulo', subtitulo}) => {
+
+  return (
+    <>
+      <h1>{ titulo }</h1>
+      <p> { subtitulo }</p>
+    </>
+  )
+}
+
+FirstApp.propTypes = { 
+  titulo: PropTypes.string.isRequired,
+  subtitulo: PropTypes.number
+}
+FirstApp.defaultProps = {
+  titulo: 'no funciona con defaultProps'
+}
+
+
---
+++ 03-counter-app-vite/src/HolaMundoApp.jsx
@@ -0,0 +1,3 @@
+export function App() {
+  return (<h1> Hola mundo1!!! </h1>);
+}
